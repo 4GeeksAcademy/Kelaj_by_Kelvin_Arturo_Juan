e9bf670183e1d81f8405ef39c67c73b44a19428e
Kelvin <kelvin.cunalema@gmail.com>
2026-09-19 17:27:37 +0000

    Fix models navbar register login

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import "../styles/Search.css"
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { useNavigate } from "react-router-dom";
+
 
 export const Navbar = () => {
+	const { store, dispatch } = useGlobalReducer();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		localStorage.removeItem("user");
+		dispatch({ type: "logout" });
+		navigate("/");
+	};
+
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
 			<div className="container-fluid px-4 px-lg-5">
@@ -14,11 +27,37 @@ export const Navbar = () => {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarContent">
 					<div className="ms-auto d-flex flex-column flex-lg-row align-items-lg-center gap-3 mt-3 mt-lg-0">
-						<a href="/" className="btn btn-outline-primary rounded-pill fw-semibold px-3 py-2">Ofrecer servicios</a>
-						<a href="#" className="text-secondary d-none d-lg-block">
-							<i className="bi bi-globe fs-5"></i>
-						</a>
-						<Link to="/Login" className="btn btn-outline-secondary rounded-pill"><i className="bi bi-person fs-5"></i>Acceder</Link>
+						{!store.user ? (
+							<>
+								<Link
+									to="/Register"
+									className="btn btn-outline-primary rounded-pill fw-semibold px-3 py-2"
+								>
+									Ofrecer servicios
+								</Link>
+
+								<Link
+									to="/Login"
+									className="btn btn-outline-secondary rounded-pill"
+								>
+									<i className="bi bi-person fs-5"></i> Acceder
+								</Link>
+							</>
+						) : (
+							<>
+								<span className="fw-semibold text-dark">
+									👤 {store.user.name}
+								</span>
+
+								<button
+									className="btn btn-outline-danger rounded-pill fw-semibold px-3 py-2"
+									onClick={handleLogout}
+								>
+									Cerrar sesión
+								</button>
+							</>
+						)}
+
 					</div>
 				</div>
 			</div>
